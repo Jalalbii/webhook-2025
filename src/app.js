924275a838ad0ea577d11ab9510dcd5c4ce082dd
Jalalbii <8451031656safa@gmail.com>
2025-06-26 21:30:34 +0530

    second commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+
+app.post("/webhook", (req, res) => {
+    console.log("Webhook received")
+    console.log("Body ===> ", req.body)
+    console.log("Event ===> ", req.headers['x-github-event'])
+    res.send("Webhook received :) ")
+})
+
+
+app.get("/",(req, res) => {
+    res.send("This is webhook project")
+})
+
+app.listen(3000, () => {
+    console.log("Listening on PORT 3000")
+})
